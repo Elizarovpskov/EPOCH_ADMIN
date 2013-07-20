e090468f77d8d64f1c5d13b661fd5a6e9234dd1b
romanta <romanta@i.ua>
2013-07-20 21:28:20 +0300

    убраны ошибочные строки из секции мар

--- epoch/modules/maps/10.php
+++ epoch/modules/maps/10.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 $res1 = mysql_query("
 SELECT player_data.PlayerName, character_data.* 
--- epoch/modules/maps/3.php
+++ epoch/modules/maps/3.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
 $res = mysql_query("SELECT player_data.PlayerName, character_data.* FROM `player_data`, `character_data` WHERE player_data.PlayerUID = character_data.PlayerUID") or die(mysql_error());
 $markers = markers_player($res, $serverworld);
